f4700c69c8fb4cf7485c6ffffe3864cff3006593
MalBourbie <MalBourbie@gmail.com>
2025-09-12 19:41:05 +0200

    redirect the user on is profile page when he goes on login or register page and is already authenticate.

--- src/app/features/authentification/authentification.component.ts
+++ src/app/features/authentification/authentification.component.ts
@@ -1,23 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
-import { Logout } from "../logout/logout.component";
 import { LoginForm } from "../../shared/components/user-interaction/form/login-form/login-form.component";
 import { RegisterForm } from "../../shared/components/user-interaction/form/register-form/register-form.component";
+import { AuthService } from "../../service/auth.service";
 
+/**
+ * Render the login form or register form. 
+ */
 @Component({
-    imports: [LoginForm, RouterLink, Logout, RegisterForm],
+    imports: [LoginForm, RouterLink, RegisterForm],
     selector: "ab-authentification",
     templateUrl: "./authentification.component.html",
     styleUrl: "./authentification.component.scss"
 })
-
 export class Authentification {
+    authService = inject(AuthService);
+    router = inject(Router);
+
     protected url: string;
     protected title: string;
 
     constructor(router: Router) {
         this.url = router.url;
         this.title = this.setTitle();
+    }
+
+    ngOnInit() {
+        if (this.authService.isAuthenticated) {
+            this.router.navigate(["/profile"]);
+        }
     }
 
     isOnPage(url: string) {
--- src/app/interceptor/not-authorized-interceptor.ts
+++ src/app/interceptor/not-authorized-interceptor.ts
@@ -4,6 +4,13 @@ import { catchError, Observable, tap, throwError } from "rxjs";
 import { AuthService } from "../service/auth.service";
 import { Router } from "@angular/router";
 
+/**
+ * If a 401 http code is send by the API, clear the session and navigate
+ * the user to the login page.
+ * @param req 
+ * @param next 
+ * @returns 
+ */
 export function notAuthorizedInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
     const authService = inject(AuthService);
     const router = inject(Router);
@@ -11,7 +18,6 @@ export function notAuthorizedInterceptor(req: HttpRequest<unknown>, next: HttpHa
     return next(req).pipe(
         catchError((err: HttpErrorResponse) => {
             if (!err.url?.endsWith("/login") && err.status === 401) {
-                console.log("Disconnected");
                 authService.clearSession();
                 router.navigate(["/login"]);
             }
